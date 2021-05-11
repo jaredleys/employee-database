7362461d8081355ca1fc1d10053368c9fadf07dc
jaredleys <jaredleys@gmail.com>
2021-05-11 15:33:59 -0400

    Update schema

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -25,7 +25,8 @@ CREATE TABLE employees (
 CREATE TABLE salary (
     emp_no INT NOT NULL,
     salary INT NOT NULL,
-    PRIMARY KEY (emp_no)
+    PRIMARY KEY (emp_no,salary),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE dept_manager (
@@ -40,6 +41,5 @@ CREATE TABLE dept_employees (
     emp_no INT NOT NULL,
     dept_no VARCHAR(4) NOT NULL,
     PRIMARY KEY (emp_no,dept_no),
-    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
