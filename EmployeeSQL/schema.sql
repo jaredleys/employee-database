255d57839480eeda37e0c75f12c60188323eb943
jaredleys <jaredleys@gmail.com>
2021-05-11 22:49:43 -0400

    Added more queries, updated schema

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -41,5 +41,6 @@ CREATE TABLE dept_employees (
     emp_no INT NOT NULL,
     dept_no VARCHAR(4) NOT NULL,
     PRIMARY KEY (emp_no,dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
